494e09fc1404c01b2c6dc5e1602ed85cb9fc7047
Antoine BUREAU <abureau@e1r3p2.42.fr>
2016-11-20 14:49:10 +0100

    fix leaks

--- src/new_msh.c
+++ src/new_msh.c
@@ -6,7 +6,7 @@
 /*   By: abureau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/19 12:00:40 by abureau           #+#    #+#             */
-/*   Updated: 2016/11/20 13:53:13 by abureau          ###   ########.fr       */
+/*   Updated: 2016/11/20 14:48:45 by abureau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_msh	*new_msh(void)
 	tmp = (t_msh*)ft_memalloc(sizeof(t_msh));
 	if (!tmp)
 		return (NULL);
+	tmp->env = NULL;
 	tmp->env = (char**)ft_memalloc(sizeof(char*));
-	tmp->env = (NULL);
 	return (tmp);
 }
